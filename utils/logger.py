5c7aca1c0f588e3b5ea9ddd89ca000997a36edd8
z89pro <z89459377@gmail.com>
2025-12-03 23:54:47 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,6 +1,7 @@
 from loguru import logger
 import sys
 
+# Reset logger to be clean
 logger.remove()
 logger.add(sys.stdout, colorize=True, format="<green>{time:HH:mm:ss}</green> | <level>{message}</level>")
 
