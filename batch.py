61d83b7c1276f4c1acb232c0d1ef6eddaae78cd6
z89pro <z89459377@gmail.com>
2025-12-03 17:18:20 +0530

    Update batch.py

--- batch.py
+++ batch.py
@@ -1,1 +1,19 @@
 
+from progress import progress_callback
+
+start_time = time.time()
+last_update = {"time": 0, "percent": 0}
+task_id = str(uuid.uuid4())  # generate unique task id
+
+await progress_callback(
+    current_bytes,
+    total_bytes,
+    progress_msg,   # message object from send_message()
+    start_time,
+    index,
+    total_files,
+    "Downloading",  # or "Uploading"
+    filename,
+    last_update,
+    task_id
+)
