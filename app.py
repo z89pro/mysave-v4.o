7a563a19bfc74a871b40645684ec2d811a7437b0
z89pro <z89459377@gmail.com>
2025-12-03 23:58:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import os
-import datetime
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def welcome():
-    return render_template("welcome.html", version="4.0", year=datetime.datetime.now().year)
+def home():
+    return "Bot is Running | Powered by Zain"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
